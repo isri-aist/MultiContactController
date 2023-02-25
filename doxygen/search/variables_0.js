722355c2ee4042a83a2ef6c68576cdaa673eaf5b
Masaki Murooka <m-murooka@aist.go.jp>
2023-02-25 12:23:55 +0000

    Update Doxygen HTML files from commit 626ecc0c1bead08f91a2534361f27fb112aba45a

--- doxygen/search/variables_0.js
+++ doxygen/search/variables_0.js
@@ -5,8 +5,8 @@ var searchData=
   ['actualcentroidalvel_345',['actualCentroidalVel',['../structMCC_1_1CentroidalManager_1_1ControlData.html#ac224c1054040c264b2057d726336cdcf',1,'MCC::CentroidalManager::ControlData']]],
   ['actualcentroidalwrench_346',['actualCentroidalWrench',['../structMCC_1_1CentroidalManager_1_1ControlData.html#a94b5f769de3553944c80636036f56d50',1,'MCC::CentroidalManager::ControlData']]],
   ['actualzmp_347',['actualZmp',['../structMCC_1_1CentroidalManager_1_1ControlData.html#a640e902d8b891edabc75577c7129f125',1,'MCC::CentroidalManager::ControlData']]],
-  ['angulargaind_348',['angularGainD',['../structMCC_1_1CentroidalManagerDDP_1_1Configuration.html#a4ca343d755e00e5eb9e0862f2c271cfe',1,'MCC::CentroidalManagerDDP::Configuration']]],
-  ['angulargainp_349',['angularGainP',['../structMCC_1_1CentroidalManagerDDP_1_1Configuration.html#ac870b44e408aa5fccab22a428bd14b38',1,'MCC::CentroidalManagerDDP::Configuration']]],
+  ['angulargaind_348',['angularGainD',['../structMCC_1_1CentroidalManagerDDP_1_1Configuration.html#a4b49eeab8ecd9aabc74ec42037c5afc6',1,'MCC::CentroidalManagerDDP::Configuration']]],
+  ['angulargainp_349',['angularGainP',['../structMCC_1_1CentroidalManagerDDP_1_1Configuration.html#af8ecad6d0a82c234809e5e26adb550a9',1,'MCC::CentroidalManagerDDP::Configuration']]],
   ['approachdurationratio_350',['approachDurationRatio',['../structMCC_1_1SwingTrajCubicSplineSimple_1_1Configuration.html#a159cf3976be297875ee67f51dc3a71b7',1,'MCC::SwingTrajCubicSplineSimple::Configuration']]],
   ['approachoffset_351',['approachOffset',['../structMCC_1_1SwingTrajCubicSplineSimple_1_1Configuration.html#a314d2ff0062786ba7f32967cb6e97733',1,'MCC::SwingTrajCubicSplineSimple::Configuration']]]
 ];
