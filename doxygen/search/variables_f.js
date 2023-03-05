d34bf75ad03b77c5368bf5b467d813b41c3f23a9
Masaki Murooka <m-murooka@aist.go.jp>
2023-03-05 01:38:42 +0000

    Update Doxygen HTML files from commit 9a70aa5f2b977612e0ada9706ed604db8446b8d3

--- doxygen/search/variables_f.js
+++ doxygen/search/variables_f.js
@@ -5,6 +5,6 @@ var searchData=
   ['requireimpgainupdate_5f_448',['requireImpGainUpdate_',['../classMCC_1_1LimbManager.html#a14bc2a6e82b007c72157b49e2f87d5f3',1,'MCC::LimbManager']]],
   ['rightfoot_449',['rightFoot',['../structMCC_1_1GuiWalkState.html#a9a8bd62d4791c65b50f531422be029e0',1,'MCC::GuiWalkState']]],
   ['robotmass_5f_450',['robotMass_',['../classMCC_1_1CentroidalManager.html#a3ad2e87a8bf3fa53669b9c65289f7e2e',1,'MCC::CentroidalManager']]],
-  ['robotmomentofinertia_5f_451',['robotMomentOfInertia_',['../classMCC_1_1CentroidalManagerPC.html#a114981958b153d283bcc3d68939eb969',1,'MCC::CentroidalManagerPC']]],
+  ['robotmomentofinertia_5f_451',['robotMomentOfInertia_',['../classMCC_1_1CentroidalManager.html#a6775a04cdc3cb16b6042f99a22462345',1,'MCC::CentroidalManager']]],
   ['rotfunc_5f_452',['rotFunc_',['../classMCC_1_1SwingTrajCubicSplineSimple.html#aae4f11c4327fbbc60e90fd89a20d073b',1,'MCC::SwingTrajCubicSplineSimple']]]
 ];
