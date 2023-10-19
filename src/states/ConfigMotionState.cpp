a734c7bae9ac30df34f632204120cd56fdd51a8a
Iori Kumagai <iori-kumagai@aist.go.jp>
2023-10-19 12:47:51 +0900

    Check limb swing motion has been finished at the end of ConfigMotion if exitWhenLimbSwingFinished is true

--- src/states/ConfigMotionState.cpp
+++ src/states/ConfigMotionState.cpp
@@ -105,6 +105,12 @@ void ConfigMotionState::start(mc_control::fsm::Controller & _ctl)
     }
   }
 
+  // Set option to wait for finishing swing motion
+  if(config_.has("configs") && config_("configs").has("exitWhenLimbSwingFinished"))
+  {
+    exitWhenLimbSwingFinished_ = static_cast<bool>(config_("configs")("exitWhenLimbSwingFinished"));
+  }
+
   output("OK");
 }
 
@@ -177,7 +183,13 @@ bool ConfigMotionState::run(mc_control::fsm::Controller &)
     }
   }
 
-  return !ctl().limbManagerSet_->contactCommandStacked() && taskConfigList_.empty() && collisionConfigList_.empty();
+  bool limbCofingsFinshed = !ctl().limbManagerSet_->contactCommandStacked();
+  if(exitWhenLimbSwingFinished_)
+  {
+    limbCofingsFinshed = limbCofingsFinshed && !ctl().limbManagerSet_->isExecutingLimbSwing();
+  }
+
+  return limbCofingsFinshed && taskConfigList_.empty() && collisionConfigList_.empty();
 }
 
 void ConfigMotionState::teardown(mc_control::fsm::Controller &) {}
