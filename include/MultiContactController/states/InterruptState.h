3ca62cd8523500fd86e5cb3f02adad5b4cb6e65e
Iori Kumagai <iori-kumagai@aist.go.jp>
2024-04-03 13:27:52 +0900

    Add InterruptState to stop multi-contact motion until an user allows to proceed

---
+++ include/MultiContactController/states/InterruptState.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <TrajColl/CubicInterpolator.h>
+
+#include <MultiContactController/State.h>
+
+namespace MCC
+{
+/** \brief FSM state to initialize. */
+struct InterruptState : State
+{
+public:
+  /** \brief Start. */
+  void start(mc_control::fsm::Controller & ctl) override;
+
+  /** \brief Run. */
+  bool run(mc_control::fsm::Controller & ctl) override;
+
+  /** \brief Teardown. */
+  void teardown(mc_control::fsm::Controller & ctl) override;
+
+protected:
+  /** \brief Check whether state is completed. */
+  bool complete() const;
+
+protected:
+  //! Phase
+  int phase_ = 0;
+
+  //! BaseTime for automatic start
+  double baseTime_ = 0.0;
+};
+} // namespace MCC
